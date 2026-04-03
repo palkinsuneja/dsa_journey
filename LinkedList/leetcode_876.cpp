89d8f427ff51874c5ad0994d039fefcd648c8706
palkinsuneja <palkinsuneja@gmail.com>
2026-04-03 17:21:25 +0530

    Add method to find the middle node of a linked list
    Implement middleNode function to find the middle of a linked list using the tortoise and hare approach.
    Added solution for LeetCode 876 - Middle of the Linked List
    
    Approach:
    - Brute force using length calculation
    - Optimized using tortoise and hare (slow-fast pointers)
    
    Part of my DSA journey 🚀

--- LinkedList/leetcode_876.cpp
+++ LinkedList/leetcode_876.cpp
@@ -1,1 +1,33 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* middleNode(ListNode* head) {
+        ListNode* tortoise = head; //slow pointer
+        ListNode* hare = head;  //fast pointer
+        while(hare != NULL && hare->next != NULL){
+            /* 🟢 One-line logic:
 
+            👉 fast->next check = crash prevention
+            👉 fast check = loop termination
+            */
+            tortoise = tortoise -> next;
+            hare = hare -> next -> next;
+        }
+        return tortoise;
+    }
+};
+/* BRUTE 
+Step 1: Traverse → find length (len)
+Step 2: mid = len / 2
+Step 3: Traverse again till mid
+Step 4: return that node
+*/
